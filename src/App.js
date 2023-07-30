cbf599a23c1edea7e20556b81336da6f89dc295e
AanandaKhadka12 <aanandakhadka12@gmail.com>
2023-07-30 12:19:04 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import Navbar from './Components/Navbar'
+import Carousel from './Components/Carousel'
+import About from './Components/About'
+import Footer from './Components/Footer'
 
 const App = () => {
   return (
-    <div>Hello From world!!!</div>
 
+    <>
+      <Navbar />
+      <Carousel />
+      <About />
+      <Footer/>
+    </>
+    
+    
   )
 }
 
